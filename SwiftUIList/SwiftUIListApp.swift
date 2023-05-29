328853a96c115207e0e3b919632c6ae1898019b5
jgamer-playzyt <jackmorley.uk@gmail.com>
2023-05-29 16:45:17 +0100

    Initial Commit

---
+++ SwiftUIList/SwiftUIListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIListApp.swift
+//  SwiftUIList
+//
+//  Created by Jack Morley on 29/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
